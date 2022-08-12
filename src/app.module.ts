42f4e4f2c5a80dbaf6a997d3166ba125bc11cd6c
FBrandex <felipe.brandex@gmail.com>
2022-08-12 16:03:58 -0300

    frist commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'xprop',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    UserModule,
+    AuthModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
